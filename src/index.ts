f535f4bc7b57d827ac35ad9355472c14af9ad927
markthree <1801982702@qq.com>
2022-04-17 23:16:59 +0800

    feat: add setNpmRegistry

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { resolve } from 'path'
-const foo = 1
+import { promisify } from 'util'
+import { exec } from 'child_process'
 
-export { resolve, foo }
+const registrys = {
+	origin: 'https://registry.npmjs.org',
+	taobao: 'https://registry.npm.taobao.org'
+}
+
+export const setNpmRegistry = async (
+	type: keyof typeof registrys
+) => {
+	const execPromisify = promisify(exec)
+	const registry = registrys[type] || registrys.origin
+	try {
+		await execPromisify(
+			`npm config set registry ${registry}`
+		)
+		return true
+	} catch (error: any) {
+		console.log(error.message)
+		return false
+	}
+}
